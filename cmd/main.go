90ba0201a9ea82536569775f422f16053a673239
laurence <laurence.aung@superbet.com>
2022-04-22 16:03:15 +0100

    graceful shutdown

--- cmd/main.go
+++ cmd/main.go
@@ -1,13 +1,41 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 )
 
 func main() {
 	fmt.Println("app starting ..")
 
 	r := setupRoutes()
-	fmt.Println("service listening on port 8080")
-	r.Run()
+	srv := &http.Server{
+		Addr:    ":8080",
+		Handler: r,
+	}
+
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			fmt.Printf("listen: %s\n", err)
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+
+	fmt.Println("Shutting down server...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5 * time.Second)
+	defer cancel()
+	if err := srv.Shutdown(ctx); err != nil {
+		fmt.Printf("Server forced to shutdown: %s\n", err)
+	}
+
+	fmt.Println("Server exiting")
 }
